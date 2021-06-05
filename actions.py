304ed802b2734763022453964d283a87038c3a2b
Varunr1995 <varunkumarrachamalla@live.com>
2021-06-05 19:50:34 +0530

    Added form & connected to actions.py

--- actions.py
+++ actions.py
@@ -2,5 +2,23 @@ from typing import Dict, Text, Any, List, Union
 
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.forms import FormValidationAction, FormAction
 
+class Action_Painting_Form(Action):
+    def name(self) -> Text:
+        return "Action_Painting_Form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """ List of required slots that the form has to fill (inputs from the user) """
+        print("required_slots(tracker: Tracker")
+        return["name", "entity_2", "entity_3", "entity_4", "entity_5", "entity_6"] # The list how we provide here will reflect in the sequence of questions from the bot as form.
+
+
+    def submit(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict]:
+
+        dispatcher.utter_message(template="utter_submit")
+
+        return[]
