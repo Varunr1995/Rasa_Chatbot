96aef749b8036a61592e3ba7daa70bd38f7742a8
Varunr1995 <varunkumarrachamalla@live.com>
2021-06-05 12:35:14 +0530

    First need to create a form to get the details from user as Input, the collected I/P need to be stored in database

---
+++ actions.py
@@ -0,0 +1,6 @@
+from typing import Dict, Text, Any, List, Union
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+
